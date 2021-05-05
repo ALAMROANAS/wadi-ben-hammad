851a3a115d843043978d8014116e6077e0ff98bb
Mrsycho <>
2021-05-05 12:34:36 +0000

    alert

---
+++ app.js
@@ -0,0 +1,12 @@
+// this code is for showing a message for the user
+
+alert ('wellcome Tourist')
+
+var firsttime =prompt('Have you visited wadi ben hammad?')
+if (firsttime == 'yes'){
+  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWEGi4Jk9vgO2Sdd12uRwvf5HAFLi1cLUnkg&usqp=CAU">')
+}else if(firsttime ==='no'){
+  document.write('<img src="https://pvt.jo/uploads/filemanager/Photos/Wadi%20Hammad/Wadi%20Ibn%20Hammad%203site_800x500.jpg">')
+}else {
+  alert ('ok you must visit it')
+  }
